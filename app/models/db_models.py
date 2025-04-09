fe9e7f1e7120b0b2b06dec1e2e22225f2d5468e0
Alexey <79776177342@yandex.ru>
2025-04-09 15:07:48 +0300

    init

---
+++ app/models/db_models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, BigInteger, \
+ForeignKey, String, Double, DateTime, Text, Table, LargeBinary
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.ext.asyncio import create_async_engine
+
+
+Base = declarative_base()
+
+class Table(Base):
+    __tablename__ = 'tables'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(25))
+    seats = Column(Integer)
+    location = Column(Text)
+
+
+class Reservation(Base):
+    __tablename__ = 'reservations'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    customer_name = Column(Text)
+    table_id = Column(Integer, ForeignKey('tables.id'))
+    reservation_time = Column(DateTime)
+    duration_minutes = Column(Integer)
